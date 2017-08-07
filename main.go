c7a7d406e4d045ce723b449a760760d7beda10f4
Kevin Menhinick <kamenhinick@gmail.com>
2017-08-07 14:17:56 +1200

    Must do uni work now

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/googollee/go-socket.io"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/firmata"
-	"github.com/googollee/go-socket.io"
 )
 
 const (
@@ -17,18 +17,57 @@ const (
 )
 
 func main() {
-	
-	go Serve()
 
 	firmataAdaptor := firmata.NewTCPAdaptor("192.168.0.20:3030")
 	servo := gpio.NewServoDriver(firmataAdaptor, "0")
+	led := gpio.NewLedDriver(firmataAdaptor, "1")
 
 	work := func() {
-		gobot.Every(1*time.Second, func() {
-			i := uint8(gobot.Rand(180))
-			fmt.Println("Turning", i)
-			servo.Move(i)
+		boiling := false
+		server, err := socketio.NewServer(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		server.On("connection", func(so socketio.Socket) {
+			led.Off()
+			so.Join("jug")
+
+			if boiling {
+				so.Emit("boiling")
+			}
+
+			so.On("disconnection", func() {
+				fmt.Println("Seeya!")
+				led.On()
+			})
+
+			so.On("boil", func() {
+				if !boiling {
+					boiling = true
+					server.BroadcastTo("jug", "boiling", nil)
+					fmt.Println("Turning on...")
+					servo.Move(uint8(30))
+					gobot.After(time.Second*5, func() {
+						fmt.Println("Turning off...")
+						servo.Move(uint8(120))
+						server.BroadcastTo("jug", "boiled", nil)
+						server.BroadcastTo("jug", "message", "JUG: I'm ready!")
+						boiling = false
+					})
+				}
+			})
 		})
+		server.On("error", func(so socketio.Socket, err error) {
+			log.Println("error:", err)
+		})
+
+		fs := http.FileServer(http.Dir("www"))
+		http.Handle("/", fs)
+		http.Handle("/socket.io/", server)
+
+		log.Println("Listening...")
+		log.Fatal(http.ListenAndServe(addr, nil))
 	}
 
 	robot := gobot.NewRobot("bot",
@@ -38,30 +77,4 @@ func main() {
 	)
 
 	robot.Start()
-}
-
-func Serve() {
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-		so.Join("chat")
-		so.On("chat message", func(msg string) {
-			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
-		})
-		so.On("disconnection", func() {
-			log.Println("on disconnect")
-		})
-	})
-
-	fs := http.FileServer(http.Dir("www"))
-	http.Handle("/", fs)
-	http.Handle("/socket.io/", server)
-
-	log.Println("Listening...")
-	http.ListenAndServe(addr, nil)
 }
--- www/script.js
+++ www/script.js
@@ -1,26 +1,54 @@
 var boiling = false;
 var socket = null;
+var room = 'jug';
 
 $(document).ready(function() {
     socket = io();
 
     socket.on('connect', function() {
-        alert('connected!');
-    });
+        console.log('connected.');
+    })
+
+    socket.on('boiled', onBoiled);
+    socket.on('boiling', disableButton);
+
+    socket.on('message', function(msg) {
+        console.log(msg);
+    })
 
     $('.fill-y').css({ height: $(window).height() });
 
-    $('.btn.boil').click(function() {
-        boil(this);
-    });
+    $('.btn.boil').click(boil);
 });
 
-function boil(btn) {
-    boiling = true;
-    $(btn).addClass('disabled');
-    $(btn).children('i').html('grain').css({ animation: 'fadeIn 1s infinite alternate'});
+function onBoiled() {
+    console.log('boiled.');
+    boiling = false;
+    $('.btn.boil')
+        .removeClass('disabled')
+        .children('i')
+        .html('local_drink')
+        .css({ animation: 'none'});
+    
+    setTimeout(function() {
+        location.reload();
+    }, 1000);
+}
 
-    dots($(btn).children('span'), 'Boiling');
+function boil() {
+    console.log('boiling..');
+    console.log(socket.emit('boil'));
+    disableButton();
+}
+
+function disableButton() {
+    boiling = true;
+    $('.btn.boil')
+        .addClass('disabled')
+        .children('i').html('grain')
+        .css({ animation: 'fadeIn 1s infinite alternate'});
+
+    dots($('.btn.boil').children('span'), 'Boiling');
 }
 
 function dots(obj, str, n) {
@@ -30,5 +58,9 @@ function dots(obj, str, n) {
         setTimeout(function() {
             dots(obj, str, (n < 3)? (n + 1): 0);
         }, 200);
+    } else {
+        $('.btn.boil')
+            .children('span')
+            .html('Boil!');
     }
 }
